76f74d359dd51d125263c53a87d25f257cca0c00
Julio Corbaz <julio.corbaz@gmail.com>
2021-10-24 17:11:26 -0300

    vite-svelte-tailwind-daisyui-mongodb.git

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{svelte,js,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
